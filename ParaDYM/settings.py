66f6594ca0097df7fd9007413037041adf17fe30
LEE-RUM <lirimehmeti22@gmail.com>
2022-02-01 13:51:35 -0500

    just testing

--- ParaDYM/settings.py
+++ ParaDYM/settings.py
@@ -127,3 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #STATIC_ROOT = BASE_DIR / 'static' #Online
 STATICFILES_DIRS = ['static']  # On Compute
 ###############################################################################
+
+#a test
